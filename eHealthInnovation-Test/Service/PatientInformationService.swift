4fb0800090d985d30a3158f3e9ec7dcf7cffe38f
Paul Huynh <paul.huynh@aircanada.ca>
2020-11-26 12:50:34 -0500

    Revert "support/setup files for the base project"
    This reverts commit 579ed6e3704cb281577dd46a5c7e95369880d9ee.

--- eHealthInnovation-Test/Service/PatientInformationService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PatientInformationService.swift
-//  eHealthInnovation-Test
-//
-//  Created by Paul Huynh on 2020-11-26.
-//  Copyright © 2020 Paul Corp. All rights reserved.
-//
-
-import Foundation
